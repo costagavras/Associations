eee25d67b80f3a678bf7635617d4b1a96556f8c0
costagavras <cgavras@yahoo.ca>
2018-10-02 21:46:45 -0400

    Ticketmaster

--- Ticketmaster/app/models/person.rb
+++ Ticketmaster/app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+  has_many :tickets
+  has_many :events, through: :tickets
 end
--- Ticketmaster/app/models/ticket.rb
+++ Ticketmaster/app/models/ticket.rb
@@ -1,2 +1,4 @@
 class Ticket < ApplicationRecord
+   belongs_to :people
+   belongs_to :event
 end
